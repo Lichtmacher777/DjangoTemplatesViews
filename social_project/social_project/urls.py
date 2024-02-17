f245193de713754f2a0c543576b0b092698ea7a7
Sergii Ponomarenko <133200566+Lichtmacher777@users.noreply.github.com>
2024-02-17 16:58:09 +0100

    Update urls.py

--- social_project/social_project/urls.py
+++ social_project/social_project/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('app/',include('social_app.urls_1')), # class based
     #path('',include('social_app.urls')), # function based
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
