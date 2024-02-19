4391ef1068da10509268dc63cf2f72fadab2cfe8
Sergii Ponomarenko <133200566+Lichtmacher777@users.noreply.github.com>
2024-02-19 16:54:44 +0100

    Update urls.py

--- social_project/social_project/urls.py
+++ social_project/social_project/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/',include('social_app.urls_1')), # class based
+    path('',include('social_app.urls_1')), # class based
     #path('',include('social_app.urls')), # function based
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
